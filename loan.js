59cb41755c432441aec116f2c771fe63a674f0e3
Noidea22 <mathewsteenwinkel@gmail.com>
2023-03-17 10:27:30 -0600

    made loan .js

---
+++ loan.js
@@ -0,0 +1,21 @@
+let creditLimit = 5000;
+
+const loanOut = function(amount) {
+  return new Promise((resolve, reject) => {
+    if (creditLimit <= 0) {
+      reject("Insufficient Funds!");
+    } else { 
+      creditLimit -= amount;
+      resolve(amount);
+    }
+  });
+};
+
+console.log("Asking for $150, which should be okay ...");
+loanOut(150)
+  .then((amountReceived) => {
+    console.log(`\t-> I got $${amountReceived} loan from the bank! Remaining Credit Limit: $${creditLimit}`);
+  })
+  .catch((err) => {
+    console.log(`\t-> Error: ${err}!`);
+  });
